f2e159fc227e5fd460b63bbb411c90e3f7c0039b
Jamal <jamal.l.mcdowell@gmail.com>
2016-08-19 10:02:05 -0400

    inital commit, may need to install zip file if java ask for it

---
+++ EncryptMessage.java
@@ -0,0 +1,41 @@
+package com.encryption;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptMessage {
+
+	public static String EncryptMe(String userMessage, Key myKey) {
+		byte[] userEncryptedMessage = null;
+		String userMessageInBytes = null;
+		String userMessageInChars = null;
+		try {
+
+			Key userAESKey = new SecretKeySpec(myKey.getEncoded(), "AES");
+			Cipher userCipher = Cipher.getInstance("AES");
+
+			userCipher.init(Cipher.ENCRYPT_MODE, userAESKey);
+
+			userEncryptedMessage = userCipher.doFinal(userMessage.getBytes());
+			userMessageInBytes = Arrays.toString(userEncryptedMessage);
+			userMessageInChars = new String(userEncryptedMessage);
+
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException
+				| BadPaddingException e) {
+			e.printStackTrace();
+		}
+
+		return "Your message in characters is: \n" + userMessageInChars + "\nYour message in numbers is: \n"
+				+ userMessageInBytes;
+
+	}
+
+}
---
+++ MyMessage.java
@@ -0,0 +1,23 @@
+package com.encryption;
+
+import java.security.Key;
+import java.util.Scanner;
+
+public class MyMessage {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Key myKey = SecretKeyGenerator.myKeyGen();
+
+		System.out.println("Please Enter The Message You Would Like To Keep Secret:");
+
+		String userMessage = sc.nextLine();
+
+		System.out.println(EncryptMessage.EncryptMe(userMessage, myKey));
+
+		sc.close();
+	}
+
+	
+}
